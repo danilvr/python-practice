a47b3f8e2177c872db502d0036d5995805392f16
Danil Vorozheykin <danilvda@gmail.com>
2021-03-17 22:43:18 +0300

    Add Extra Practice 3

---
+++ extra_practice03/part1/import_all.py
@@ -0,0 +1,11 @@
+from extra_practice03.part1 import *
+
+func1()
+func2()
+
+# function not imported
+# func3()
+
+from extra_practice03.part1 import func3
+
+func3()
---
+++ extra_practice03/part1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='study_package',
+    version='0.0.1',
+    description='Study package',
+    author='Vorozheykin Danil',
+    author_email='vorozheykin.d.a@edu.mirea.ru',
+    packages=['study_package']
+)
